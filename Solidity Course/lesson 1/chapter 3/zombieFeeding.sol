4e0fad29daae99e530b5823c53b7836d36029c98
Aytaditya <adityaaryan531@gmail.com>
2024-06-02 03:45:15 +0530

    chapter 3 updated

--- Solidity Course/lesson 1/chapter 3/zombieFeeding.sol
+++ Solidity Course/lesson 1/chapter 3/zombieFeeding.sol
@@ -19,22 +19,32 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  // 1. Remove this:
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  // 2. Change this to just a declaration:
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
 
-  // 3. Add setKittyContractAddress method here
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
 
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+  // 1. Make this function internal
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+    // 2. Add a check for `_isReady` here
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
+    // 3. Call `_triggerCooldown`
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
