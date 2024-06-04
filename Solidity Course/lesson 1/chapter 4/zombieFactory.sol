32534802eef0684114beaf526df695794f4aca88
Aytaditya <adityaaryan531@gmail.com>
2024-06-05 00:06:13 +0530

    Chapter 4 completed

--- Solidity Course/lesson 1/chapter 4/zombieFactory.sol
+++ Solidity Course/lesson 1/chapter 4/zombieFactory.sol
@@ -15,7 +15,8 @@ contract ZombieFactory is Ownable {
       uint dna;
       uint32 level;
       uint32 readyTime;
-      // 1. Add new properties here
+      uint16 winCount;
+      uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -24,7 +25,7 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
